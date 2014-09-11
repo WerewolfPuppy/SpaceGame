070e0449512fb0ff633d5cc419e14ee8a425e8af
WerewolfPuppy <hushmail.com>
2014-09-11 09:31:19 +0200

    Added draw conditions & they work

--- Spaaace/Unity Project/Assets/Spaaace/Scripts/Health.cs
+++ Spaaace/Unity Project/Assets/Spaaace/Scripts/Health.cs
@@ -3,31 +3,37 @@ using System.Collections;
 
 public class Health : MonoBehaviour {
 
-    public bool Player1;
+  public bool Player1;
     
-    public bool Dead;
+  public bool dead;
 
-    public GameObject deathExplosion;
+  public GameObject deathExplosion;
 
-    void OnCollisionEnter (Collision colinfo) {
-	    if(colinfo.collider.tag == "Ast" || colinfo.collider.tag == "Missile" || colinfo.collider.tag == "Wall"){
-            Dead = true;
-            Win();
-        }
-	}
-
-    void Win()
-    {
-        GlobalPlayerData.gameOver = true;
-
-        if (GlobalPlayerData.playerInt == 0)
-        {
-            GlobalPlayerData.winner = !Player1;
-        }
-
-        Instantiate(deathExplosion, transform.position, transform.rotation);
-        Time.timeScale = 0;
-        Destroy(gameObject);
+  void OnCollisionEnter (Collision colinfo) {
+    if(colinfo.collider.tag == "Ast" || colinfo.collider.tag == "Missile" || colinfo.collider.tag == "Wall"){
+      dead = true;
+      Win();
     }
+    if(colinfo.collider.tag == "SpaceShip")
+      {
+	GlobalPlayerData.draw = true;
+	dead = true;
+	Win();
+      }
+  }
 
+  void Win()
+  {
+    GlobalPlayerData.gameOver = true;
+    if(GlobalPlayerData.draw == false)
+      {
+	if (GlobalPlayerData.playerInt == 0)
+	  {
+	    GlobalPlayerData.winner = !Player1;
+	  }
+      }
+    Instantiate(deathExplosion, transform.position, transform.rotation);
+    Time.timeScale = 0;
+    Destroy(gameObject);
+  }
 }
